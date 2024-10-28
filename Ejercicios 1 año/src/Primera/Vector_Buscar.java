0d5a934768677496438e43ae79afc37a50899db6
Juan Antonio Alfro Peña <xbluuz@gmail.com>
2024-10-28 11:16:52 +0100

    update

--- Ejercicios 1 año/src/Primera/Vector_Buscar.java
+++ Ejercicios 1 año/src/Primera/Vector_Buscar.java
@@ -63,7 +63,7 @@ System.out.println("-------------------------------------------------");
         
         
         //CEAMOS UN ARRAY DE 50 POSICIONES
-        String[] palabras = new String[50];
+        String[] palabras = new String[10];
 
         //CONSITE ES QUE EN CADA FASE DE EL ARRAY GENERE CON EL OTRO FOR 4 LETRAS CON CHAR QUE FORMEN LA PALBRA DE 4 LETRAS Y MOSTRAMOS
         for(int i=0;i<palabras.length;i++){
@@ -83,6 +83,51 @@ System.out.println("-------------------------------------------------");
         for (int i=0 ; i<palabras.length; i++){
             if (palabra_user.equals(palabras[i])){
                 System.out.println("La palabra "+palabra_user+" se encuentra en la posicion "+i);
+            }
+        }
+       
+
+        System.out.println("-------------------------------------------------");
+
+        String[] palabras2 = new String[4];
+        char[] letra1 = new char[4];
+        char[] letra2 = new char[4];
+        char[] letra3 = new char[4];
+        char[] letra4 = new char[4];
+
+        //Creamos aleatoramente las letras
+        for (int i=0 ; i< 4; i++){
+            letra1[i] = (char) Math.round(Math.random()*25+65);
+            letra2[i] = (char) Math.round(Math.random()*25+65);
+            letra3[i] = (char) Math.round(Math.random()*25+65);
+            letra4[i] = (char) Math.round(Math.random()*25+65);
+        }
+
+        for (int i=0;i<palabras2.length;i++){
+            palabras2[i] = ""+(char) letra1[i]+(char) letra2[i]+(char) letra3[i]+(char) letra4[i];
+            System.out.println("Posicion "+i +1 +" : "+palabras2[i]);
+        }
+
+        System.out.println("Introduce la primera letra: ");
+        String palabra_user2 = sc.next();
+
+        for (int i = 0; i < palabras2.length; i++) {
+            if (palabra_user2.charAt(0) == letra1[i]) {
+                System.out.println("Dime la segunda letra: ");
+                palabra_user2 = sc.next();
+                if (palabra_user2.charAt(0) == letra2[i]) {
+                    System.out.println("Dime la tercera letra: ");
+                    palabra_user2 = sc.next();
+                    if (palabra_user2.charAt(0) == letra3[i]) {
+                        System.out.println("Dime la cuarta letra: ");
+                        palabra_user2 = sc.next();
+                        if (palabra_user2.charAt(0) == letra4[i]) {
+                            System.out.println("La palabra "+palabras2[i]+"se encuentra en la posicion " + i);
+                        }
+                    }else{
+                        System.out.println("La palabra no se encuentra");
+                    }
+                }
             }
         }
         sc.close();
