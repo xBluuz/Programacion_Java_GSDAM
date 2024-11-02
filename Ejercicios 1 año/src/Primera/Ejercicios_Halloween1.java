219112a0f8097bdeb24aa54db8707da75ccc998a
xBluuz <132654466+xBluuz@users.noreply.github.com>
2024-11-02 12:23:13 +0100

    update

--- Ejercicios 1 año/src/Primera/Ejercicios_Halloween1.java
+++ Ejercicios 1 año/src/Primera/Ejercicios_Halloween1.java
@@ -11,7 +11,28 @@ public class Ejercicios_Halloween1 {
     static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Ejr1();
+        //Ejr1();
+        System.out.println("");
+        System.out.println("-------------------------------------------------");
+        //Ejr2();
+        System.out.println("");
+        System.out.println("-------------------------------------------------");
+        //Ejr3();
+        System.out.println("");
+        System.out.println("-------------------------------------------------");
+        //Ejr4();
+        System.out.println("");
+        System.out.println("-------------------------------------------------");
+        //Ejr5();
+        System.out.println("");
+        System.out.println("-------------------------------------------------");
+        //Ejr6();
+        System.out.println("");
+        System.out.println("-------------------------------------------------");
+        //Ejr7();
+        System.out.println("");
+        System.out.println("-------------------------------------------------");
+        Ejr8();
     }
 
     //Crea un programa que pida al usuario ingresar 10 números enteros y los almacene en un array.
@@ -30,6 +51,7 @@ public class Ejercicios_Halloween1 {
         System.out.println("El número menor es: "+menor(numerosEj1));
         
     }
+
     public static int promedio(int[] numeros){
         int suma = 0;
         for (int i = 0; i < numeros.length; i++){
@@ -37,6 +59,7 @@ public class Ejercicios_Halloween1 {
         }
         return suma/numeros.length;
     }
+
     public static int mayor(int[] numeros){
         int mayor = 0;
 
@@ -48,6 +71,7 @@ public class Ejercicios_Halloween1 {
         }
         return mayor;
     }
+
     public static int menor(int[] numeros){
         int menor = numeros[0];
         for (int i = 0; i < numeros.length; i++){
@@ -62,6 +86,198 @@ public class Ejercicios_Halloween1 {
     //Utiliza una función para contar el número total de caracteres en todas las palabras ingresadas.
     //Muestra cada palabra y su longitud individual junto con el total de caracteres en consola.
 
-    
+    public static void Ejr2(){
+        String[] palabrasEjr2 = new String[5];
+        for (int i= 0; i< palabrasEjr2.length; i++){
+            System.out.println("Introduce la palabra "+(i+1)+": ");
+            palabrasEjr2[i] = sc.next();
+        }
+        System.out.println("");
+        System.out.println("El total de caracteres en las palabras es: "+contarCaracteres(palabrasEjr2));
+        mostrarPalabras(palabrasEjr2);
 
-}
+    }
+
+    public static int contarCaracteres(String[] palabras){
+        int totalCaracteres = 0;
+        for (int i=0 ; i < palabras.length; i++){
+            totalCaracteres += palabras[i].length();
+        }
+        return totalCaracteres;
+    }
+
+    public static void mostrarPalabras(String[] palabras){
+        for (int i = 0; i < palabras.length; i++){
+            System.out.println("La palabra "+(i+1)+" es: "+palabras[i]+" y tiene "+palabras[i].length()+" caracteres.");
+        }
+    }
+
+    //Crea un programa que permita al usuario ingresar 10 números enteros en un array.
+    //Implementa una función que ordene el array en orden ascendente usando el método de burbuja o cualquier algoritmo de ordenación básico.
+    //Muestra el array original y el array ordenado en consola.
+
+    public static void Ejr3(){
+        int[] numeroEjr3 = new int[10];
+        for (int i= 0; i< numeroEjr3.length; i++){
+            System.out.println("Introduce el "+(i+1)+" número entero: ");
+            numeroEjr3[i] = sc.nextInt();
+        }
+        System.out.println("");
+        System.out.println("Array ordendo: ");
+        ordenarBurbuja(numeroEjr3);
+    }
+
+    public static void ordenarBurbuja(int[] numeros){
+        int cambio;
+        for (int i = 0; i < numeros.length-1; i++){
+            for (int j= i+1; j< numeros.length; j++){
+                if (numeros[i]>numeros[j]){
+                    cambio = numeros[i];
+                    numeros[i] = numeros[j];
+                    numeros[j] = cambio;
+                }
+            } 
+        }
+        for (int i = 0; i < numeros.length; i++){
+            System.out.println(numeros[i]);
+        }
+    }
+
+    //Crea un programa que tenga un array de temperaturas en grados Celsius (5 valores).
+    //Escribe una función que reciba este array y devuelva otro array con las temperaturas convertidas a Fahrenheit.
+    //Muestra ambos arrays en consola (Celsius y Fahrenheit).
+
+    public static void Ejr4(){
+        int[] temperaturas = new int[5];
+        for (int i=0 ; i< temperaturas.length; i++){
+            System.out.println("Introduce la temperatura en grados Celsius "+(i+1)+": ");
+            temperaturas[i] = sc.nextInt();
+        }
+        System.out.println("");
+        System.out.println("Temperaturas en grados Celsius: ");
+        for (int i = 0; i < temperaturas.length; i++){
+            System.out.println(temperaturas[i]+"ºC");
+        }
+        System.out.println("");
+        System.out.println("Temperaturas en grados Fahrenheit: ");
+        convertirFahr(temperaturas);
+    }
+
+    public static void convertirFahr(int[] temperaturas){
+        for (int i = 0; i < temperaturas.length; i++){
+            temperaturas[i] = (temperaturas[i]*9/5)+32;
+        }
+        for (int i = 0; i < temperaturas.length; i++){
+            System.out.println(temperaturas[i]+"ºF");
+        }
+    }
+
+    //Crea un programa que genere 10 números aleatorios entre 1 y 20 y los almacene en un array, asegurándose de que no se repitan.
+    //Usa una función para verificar que los números generados no estén duplicados en el array antes de añadirlos.
+    //Muestra el array resultante en consola.
+
+    public static void Ejr5(){
+        int[] numerosAleatorios = new int[10];
+        for (int i = 0; i < numerosAleatorios.length; i++){
+            numerosAleatorios[i] = (int) (Math.random()*20+1);
+            for (int j = 0; j < i; j++){
+                if (numerosAleatorios[i] == numerosAleatorios[j]){
+                    i--; //Si se repite el número, se resta 1 a i para que vuelva a generar otro número
+                }
+            }
+        }
+        for (int i = 0; i < numerosAleatorios.length; i++){
+            System.out.println((i+1)+" numero: "+numerosAleatorios[i]);
+        }
+    }
+
+    //Crea un programa que almacene en un array las notas de 5 estudiantes (números enteros entre 0 y 10).
+    //Escribe funciones para calcular el promedio de notas y para contar cuántos estudiantes aprobaron (nota mayor o igual a 5) y cuántos reprobaron.
+    //Muestra los resultados en la consola.
+
+    public static void Ejr6(){
+        int[] notas = new int[5];
+        for (int i=0;i<notas.length;i++){
+            notas[i] = (int) (Math.random()*10);
+        }
+        System.out.println(" ");
+        for (int i=0;i<notas.length;i++){
+            System.out.println("Nota del estudiante "+(i+1)+": "+notas[i]);
+        }
+        System.out.println(" ");
+        OperacionesNotas(notas);
+    }
+
+    public static void OperacionesNotas(int[] notas){
+        int aprobaron = 0;
+        int reprobaron = 0;
+        int media = 0;
+        for (int i=0;i<notas.length;i++){
+            media += notas[i];
+            if (notas[i] >= 5){
+                aprobaron++;
+            }else{
+                reprobaron++;
+            }
+        }
+        System.out.println("La media de las notas es: "+media/notas.length);
+        System.out.println("Aprobaron: "+aprobaron);
+        System.out.println("Reprobaron: "+reprobaron);
+    }
+
+    //Crea un programa que permita al usuario ingresar 10 números en un array.
+    //Luego, pide al usuario que ingrese un número adicional y utiliza una función para verificar si el número está en el array.
+    //Si el número se encuentra en el array, indica su posición; si no, muestra un mensaje indicando que no se encontró.
+
+    public static void Ejr7(){
+        int[] numeros = new int[10];
+        for (int i=0;i<numeros.length;i++){
+            System.out.println("Introduce el "+(i+1)+" número: ");
+            numeros[i] = sc.nextInt();
+        }
+        buscarNumero(numeros);
+    }
+
+    public static void buscarNumero(int[] numeros){
+        System.out.println("Introduce un número para buscar en el array: ");
+        int num = sc.nextInt();
+        for (int i=0;i<numeros.length;i++){
+            if (num == numeros[i]){
+                System.out.println("El número "+num+" se encuentra en la posición "+(i+1));
+            }else{
+                System.out.println("El número no se encuentra en el array.");
+            }
+        }
+    } 
+    
+    //Crea un array de 10 números enteros y permite al usuario ingresar valores para cada posición.
+    //Usa una función que reciba el array e intercambie los elementos en las posiciones pares con los de las posiciones impares.
+    //Muestra el array antes y después del intercambio en consola.
+
+    public static void Ejr8(){
+        int[] numero = new int[10];
+        for (int i=0 ; i< numero.length; i++){
+            System.out.println("Introduce el "+(i+1)+" número: ");
+            numero[i] = sc.nextInt();
+        }
+        System.out.println("");
+        System.out.println("Array original: ");
+        for (int i=0;i<numero.length;i++){
+            System.out.println(numero[i]);
+        }
+        System.out.println("Array intercambiado: ");
+        intercambiarPosiciones(numero);
+    }
+
+    public static void intercambiarPosiciones(int[] numeros){
+        int cambio;
+        for (int i=0;i<numeros.length-1;i+=2){ //Esto es un for de los numeros impares del array
+            cambio = numeros[i];
+            numeros[i] = numeros[i+1];
+            numeros[i+1] = cambio;
+        }
+        for (int i=0;i<numeros.length;i++){
+            System.out.println(numeros[i]);
+        }
+    }
+} 
