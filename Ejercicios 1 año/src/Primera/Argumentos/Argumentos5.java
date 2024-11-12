a046f875e54bb9a5e99d1552f87b4cb7b8664427
xBluuz <132654466+xBluuz@users.noreply.github.com>
2024-11-12 19:02:34 +0100

    upsdate

--- Ejercicios 1 año/src/Primera/Argumentos/Argumentos5.java
+++ Ejercicios 1 año/src/Primera/Argumentos/Argumentos5.java
@@ -7,21 +7,31 @@ package Primera.Argumentos;
 
 public class Argumentos5 {
 
+    //Ejercicio: Suma de arguemntos de linea de comandos
+    //
+    //Escribe un progrma en java que reciba una serie de numeros por linea de comandos y los sume.
+    //Si alguno de los argumentos no es un numero, el programa debe mostrar un mensaje de error y termina.
+
+    //Requisitos:
+    //El progrma dewbe recibir los numeros argumetos de linea de comandos
+    //si el usuario no introduce ningun argumento, el programa debe mostrar un mensaje de que tiene que introducir un numero  
+    //Si todos los arguemntos son validos, el programa debe mostrar la suma de todos los numeros y si no, mostrar un mensaje de error
+
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
 
-        if (args.length != 0){
-            int numero;
-            for(int i = 0; i< args.length;i++){
-                if (Boolean.parseBoolean(numero = Integer.parseInt(args[i])));
-            }
-
-
+        if (args.length == 0){
+            System.out.println("Tienes que introducir un numero");
+            System.exit(0); 
         }else{
-            System.out.println("Debes ingresar un numero");
+            
         }
+        
+
+        
 
 
     }
